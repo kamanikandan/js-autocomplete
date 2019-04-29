2c707d6d8edabe52725138b9084cbe5a98a63e73
Manikandan Kaliyappan <MK00547145@techmahindra.com>
2019-04-29 17:59:02 +0530

    Added functionality of autocomplete

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let searchText = document.getElementById("searchText");
+let searchResults = document.getElementById("searchResults");
+let countries = [];
+fetchCountries();
+//Fetch Countries
+async function fetchCountries() {
+  let response = await fetch("../data/countries.json");
+  let results = await response.json();
+  countries = results;
+}
+//Search Country
+let searchCountries = async searchText => {
+  let regEx = new RegExp(`^${searchText}`, "gi");
+  let filteredCountries = countries.filter(country => {
+    return (
+      country.countryName.match(regEx) ||
+      country.countryCode.match(regEx) ||
+      country.continentName.match(regEx)
+    );
+  });
+  const renderHtml = filteredCountries
+    .map(country => {
+      return `
+        <div class="country-row">
+            <h2><span>${country.countryCode}:</span> ${country.countryName} <span class="continent">Continent: ${country.continentName}</span></h2>
+            <p>Population: ${country.population}</p>
+        </div>
+      `;
+    })
+    .join("");
+  if (searchText.length == 0) searchResults.innerHTML = "";
+  else searchResults.innerHTML = renderHtml;
+};
+
+searchText.addEventListener("input", () => searchCountries(searchText.value));
